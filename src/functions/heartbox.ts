c75a21046be7f64d0ea4c58bea4a1fc0a3b9a27c
Josh Spicer <hello@joshspicer.com>
2024-09-03 22:48:46 -0700

    v1.0.0

--- src/functions/heartbox.ts
+++ src/functions/heartbox.ts
@@ -1,16 +1,22 @@
-import { app, HttpRequest, HttpResponseInit, InvocationContext, output, input, StorageQueueOutput, TableOutput } from "@azure/functions";
-// import { TableClient } from "@azure/data-tables";
+// (c) 2024 Josh Spicer <hello@joshspicer.com>
+// https://joshspicer.com/heartbox
+
+import { app, HttpRequest, HttpResponseInit, InvocationContext, output, input } from "@azure/functions";
+import moment from 'moment';
+
+interface StateTransition {
+    PartitionKey: string;
+    RowKey: string; // Timestamp
+    Status: string;
+}
 
 interface HeartBoxRequest {
     deviceId: number;
+    verbose?: boolean;
 }
 
-interface HeartBoxPollingRequest extends HeartBoxRequest {
-}
-
-interface HeartBoxPutRequest extends HeartBoxRequest {
-}
-
+// The device always sends THEIR deviceId
+// Eg: Box 1 with append ?deviceId=1 to every request
 function getPeerDeviceId(myDeviceId: number) {
     return myDeviceId === 0 ? 1 : 0;
 }
@@ -21,13 +27,18 @@ export async function heartbox(request: HttpRequest, context: InvocationContext)
     // const connectionString = process.env.AzureWebJobsStorage;
     // const tableClient = TableClient.fromConnectionString(connectionString, 'heartbox');
     try {
-        const params = validateBaseParams(request.query);
-
+        const params = parseAndValidateParams(request.query);
         switch (request.method.toUpperCase()) {
             case 'GET':
+                // Polls my state and my peer's state
+                // If ?verbose=true is set, also return a list of all past states for both participants
                 return handleGet(params, context);
             case 'PUT':
+                // Sets the peer's heartbox to 'on'
                 return handlePut(params, context);
+            case 'DELETE':
+                // Sets the peer's heartbox to 'off'
+                return handleDelete(params, context);
             default:
                 return {
                     status: 405,
@@ -40,18 +51,19 @@ export async function heartbox(request: HttpRequest, context: InvocationContext)
         return {
             status: 400,
             jsonBody: {
-                message: error.message
+                message: 'message' in error ? error.message : error?.toString()
             }
         };
     }
 };
 
-function validateBaseParams(queryParams: URLSearchParams) {
+function parseAndValidateParams(queryParams: URLSearchParams) {
     if (!queryParams.has('deviceId')) {
         throw new Error('Missing deviceId');
     }
 
     const deviceId = parseInt(queryParams.get('deviceId'));
+    const verbose = queryParams.get('verbose') === 'true'
 
     if (isNaN(deviceId)) {
         throw new Error('Invalid deviceId (NaN)');
@@ -59,58 +71,101 @@ function validateBaseParams(queryParams: URLSearchParams) {
 
     return {
         deviceId,
+        verbose,
     };
 }
 
-function handleGet(params: HeartBoxPollingRequest, context: InvocationContext/*, tableClient: TableClient*/): HttpResponseInit {
-    // const deviceId = params.deviceId.toString();
-    // const entities = context.extraInputs.get(tableInput);
+function handleGet(params: HeartBoxRequest, context: InvocationContext/*, tableClient: TableClient*/): HttpResponseInit {
+    const {deviceId, verbose} = params;
 
-    // let latestEntity = null;
-    // for (const entity of entities) {
-    //     if (entity.PartitionKey === deviceId) {
-    //         if (!latestEntity || entity.RowKey > latestEntity.RowKey) {
-    //             latestEntity = entity;
-    //         }
-    //     }
-    // }
+    const device0 = context.extraInputs.get(device0Input) as StateTransition[];
+    const device1 = context.extraInputs.get(device1Input) as StateTransition[];
+
+    context.debug(`device0: ${JSON.stringify(device0)}`);
+    context.debug(`device1: ${JSON.stringify(device1)}`);
+
+    let device0LastState: StateTransition | undefined = undefined;
+    for (const state of device0) {
+        context.debug(`Processing for 0 -> ${state}`)
+        if (device0LastState === undefined || new Date(state.RowKey) > new Date(device0LastState.RowKey)) {
+            device0LastState = state;
+        }
+    }
+
+    let device1LastState: StateTransition | undefined = undefined;
+    for (const state of device1) {
+        context.debug(`Processing for 1 -> ${state}`)
+        if (device1LastState === undefined || new Date(state.RowKey) > new Date(device1LastState.RowKey)) {
+            device1LastState = state;
+        }
+    }
+
+    const transitions = {transitions: {device0, device1 }};
     
-    return {
+    const self = deviceId === 0 ? device0LastState : device1LastState;
+    const peer = deviceId === 0 ? device1LastState : device0LastState;
+    context.log(`[device${deviceId}] Polled self=${self.Status}, peer=${peer.Status}`);
+
+    const selfAgo = moment(self.RowKey).fromNow();
+    const peerAgo = moment(peer.RowKey).fromNow();
+    context.log(`[device${deviceId}] Self was set on '${self.RowKey}' (${selfAgo} ago)`);
+    context.log(`[device${deviceId}] Peer was set on '${peer.RowKey}' (${peerAgo} ago)`);
+
+    return verbose ? {
         status: 200,
         jsonBody: {
-            message: `[device${params.deviceId}] polled`
-        }
-    };
+            ...transitions,
+            self,
+            peer,
+        },
+    } : { status: 200, body: `${self.Status},${peer.Status}`}
 }
 
-function handlePut(params: HeartBoxPutRequest, context: InvocationContext/*, tableClient: TableClient*/): HttpResponseInit {
-    
+function handlePut(params: HeartBoxRequest, context: InvocationContext/*, tableClient: TableClient*/): HttpResponseInit {
     // Set my peer's heartbox to 'on'
     const peerDeviceId = getPeerDeviceId(params.deviceId);
-
     context.extraOutputs.set(tableOutput, {
         PartitionKey: peerDeviceId.toString(),
         RowKey: new Date().toISOString(),
-        status: 'on',
+        Status: 'on',
     });
-
-    // TODO: Log this state change somewhere?
     
     return {
             status: 200,
             jsonBody: {
-                message: `[device${params.deviceId}] set peer's state to 'on'`
+                message: `Device ${params.deviceId} set peer's (${peerDeviceId}) state to 'on'`
         }
     };
 }
 
-// function handleDelete(params: HeartBoxRequest, context: InvocationContext/*, tableClient: TableClient*/): HttpResponseInit {}
+function handleDelete(params: HeartBoxRequest, context: InvocationContext/*, tableClient: TableClient*/): HttpResponseInit {
+    // Set my peer's heartbox to 'off
+    const peerDeviceId = getPeerDeviceId(params.deviceId);
+    context.extraOutputs.set(tableOutput, {
+        PartitionKey: peerDeviceId.toString(),
+        RowKey: new Date().toISOString(),
+        Status: 'off',
+    });
 
-// const tableInput = input.table({
-//     tableName: 'heartbox',
-//     connection: 'AzureWebJobsStorage',
-//     filter: 'PartitionKey eq {deviceId}',
-// });
+    return {
+        status: 200,
+        jsonBody: {
+            message: `Device ${params.deviceId} set peer's (${peerDeviceId}) state to 'off'`
+        }
+    };
+}
+
+const device0Input = input.table({
+    tableName: 'heartbox',
+    connection: 'AzureWebJobsStorage',
+    filter: `PartitionKey eq '0'`,
+});
+
+const device1Input = input.table({
+    tableName: 'heartbox',
+    connection: 'AzureWebJobsStorage',
+    filter: `PartitionKey eq '1'`,
+});
 
 const tableOutput = output.table({
     tableName: 'heartbox',
@@ -118,9 +173,9 @@ const tableOutput = output.table({
 });
 
 app.http('heartbox', {
-    methods: ['GET', 'PUT'],
+    methods: ['GET', 'PUT', 'DELETE'],
     authLevel: 'function',
-    // extraInputs: [table({connection: 'AzureWebJobsStorage',tableName: 'heartbox'})],
+    extraInputs: [device0Input, device1Input],
     extraOutputs: [tableOutput],
     handler: heartbox
 });
